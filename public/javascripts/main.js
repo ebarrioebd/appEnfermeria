f5310596a3186fd8ff11cea123fe1724954e716b
ebarrioebd <ebarrios.ebd@gmail.com>
2024-05-24 11:24:22 -0600

    App enfer01

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 
 
-function calcular() {
+function calcularGotas() {
     const ml = document.getElementById("ml").value;
     const equipo = document.getElementById("selectEquipo").value;
     const tiempo = document.getElementById("tiempo").value;
@@ -20,4 +20,24 @@ function calcular() {
         console.log("Resultado:", resultado, " gts");
         document.getElementById("res").innerHTML = "<h2>" + resultado.toFixed(3) + " gts</h2>";
     }
-} 
+}
+
+let menu = document.getElementById("menu");
+let header = document.getElementById("header");
+let opciones = document.getElementById("opciones");
+let ocultarBolean = false;
+
+function animar() {  
+    if (ocultarBolean==false) {
+        //header.style.height=15+"vh";  
+        opciones.style.transition="opacity .7s";
+
+        opciones.style.display = ""; 
+        ocultarBolean=true;
+    } else if(ocultarBolean==true) {
+        //header.style.height=5+"vh";  
+        opciones.style.display = "none";
+        ocultarBolean=false;
+    }
+}
+//menu.addEventListener("click", animar, false)
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("/gotas");
 });
-
+router.get("/gotas",(req,res)=>{
+  res.render('index', { file: 'gotas'});
+});
+router.get("/gotashoras",(req,res)=>{
+  res.render('index', { file: 'gotas'});
+})
 module.exports = router;
